799f5580903a7b0c0eb3b70b72b000b376ba91c0
professionalbot <110720852+professionalbot@users.noreply.github.com>
2023-06-18 16:05:47 +0500

    Adding DB

--- app/src/main/java/com/example/hafizpara/dbManager.java
+++ app/src/main/java/com/example/hafizpara/dbManager.java
@@ -1,6 +1,8 @@
 package com.example.hafizpara;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,6 +10,7 @@ import androidx.annotation.Nullable;
 
 public class dbManager extends SQLiteOpenHelper {
     private static final String dbname="dbHafiz";
+    private int a ;
 
     public dbManager(@Nullable Context context) {
         super(context, dbname, null, 1);
@@ -15,11 +18,41 @@ public class dbManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query="create table tbl_content(id integer primary key,name text,age int)";
+        sqLiteDatabase.execSQL(query);
 
     }
 
+
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String query="DROP TABLE IF EXISTS tbl_content";
+        sqLiteDatabase.execSQL(query);
+        onCreate(sqLiteDatabase);
 
+
+    }
+
+    public String addRecords(int id,String name,int age)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put("id",id);
+        cv.put("name",name);
+        cv.put("age",age);
+        float result=db.insert("tbl_content",null,cv);
+        if(result==-1){
+            return "failed";
+        }else{
+            return "Success";
+        }
+    }
+
+    public Cursor readalldatad(){
+
+        SQLiteDatabase db=this.getWritableDatabase();
+        String query="select * from tbl_content order by id desc";
+        Cursor cursor= db.rawQuery(query,null);
+        return cursor;
     }
 }
